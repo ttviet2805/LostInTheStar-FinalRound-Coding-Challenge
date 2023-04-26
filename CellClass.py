8e997b2ccb1f5e3d617c565aa1162d765e57d76d
Hoang Nghia Viet <viethoangnghia@gmail.com>
2023-04-26 11:29:38 +0700

    Init cell class

---
+++ CellClass.py
@@ -0,0 +1,58 @@
+import pygame
+
+class Cell():
+	def __init__(self, cellCoord):
+		self.cellLength = 48
+		self.cellCoord = cellCoord
+
+class ObstacleCell(Cell):
+	def __init__(self, gameScreen, cellCoord):
+		Cell.__init__(self, cellCoord)
+
+		self.gameScreen = gameScreen
+		self.backgroundImage = {
+			"Obstacle": pygame.image.load()
+		}
+
+	def DisplayBackgroundImage(self):
+		self.gameScreen.blit(self.backgroundImage["Obstacle"], self.cellCoord)
+
+class EmptyCell(Cell):
+	def __init__(self, gameScreen, cellCoord):
+		Cell.__init__(self, cellCoord)
+
+		self.gameScreen = gameScreen
+		self.cellColor = "Null"
+		self.backgroundImage = {
+			"Empty Null": pygame.image.load(),
+			"Empty Red": pygame.image.load(),
+			"Empty Blue": pygame.image.load(),
+			"Empty Green": pygame.image.load(),
+			"Empty Yellow": pygame.image.load()
+		}
+
+	def DisplayBackgroundImage(self):
+		self.gameScreen.blit(self.backgroundImage["Empty " + self.cellColor], self.cellCoord)
+
+	def ChangeCellColor(self, newColor):
+		self.cellColor = newColor
+
+
+class DestroyableCell(EmptyCell):
+	def __init__(self, gameScreen, cellCoord):
+		EmptyCell.__init__(self, gameScreen, cellCoord)
+
+		self.isDestroyed = False
+		self.backgroundImage["Obstacle"] = pygame.image.load()
+
+	def DisplayBackgroundImage(self):
+		if self.isDestroyed == False:
+			self.gameScreen.blit(self.backgroundImage["Obstacle"], self.cellCoord)
+		else:
+			EmptyCell.backgroundImage(self)
+
+	def DetroyCell(self):
+		self.isDestroyed = True
+
+
+
